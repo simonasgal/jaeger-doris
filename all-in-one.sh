22cc6d05e61200230ddfb7d5240caa70ff7658cb
composer <2789706336@qq.com>
2024-09-24 15:50:52 +0800

    fix: bash bug

--- all-in-one.sh
+++ all-in-one.sh
@@ -1,4 +1,4 @@
-#!/usr/bin/env sh
+#!/usr/bin/env bash
 export SPAN_STORAGE_TYPE=grpc
 export GRPC_STORAGE_SERVER='localhost:17271'
 
@@ -6,6 +6,12 @@ export GRPC_STORAGE_SERVER='localhost:17271'
 trap 'kill $(jobs -p)' SIGINT SIGTERM
 
 ./jaegerdoris --config $1 &
+sleep 1
+pgrep -x "./jaegerdoris" > /dev/null
+if [ $? -ne 0 ]; then
+    exit 1
+fi
+
 ./jaegerquery &
 
 wait -n
